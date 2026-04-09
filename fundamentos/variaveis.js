f54d7d6315e0452d52b667773b6cfb2572f0772b
gustavohenr1q <gugaribeiroo88@gmail.com>
2026-04-08 22:23:24 -0300

    Aula de JS Variáveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,34 @@
+/**
+ * Estudo das variáveis
+ * @author Gustavo Henrique
+ */
+ 
+console.clear()
+console.log('Estudo das variáveis')
+console.log('')
+
+// Variáveis
+let nome = 'Gustavo Henrique'
+let idade = 19
+let peso = 68
+let altura = 1.65
+
+console.log('')
+
+let vip = true
+  
+// Tabela da ficha
+console.log('')
+console.log('--------------------------------------')
+console.log('-           Ficha do aluno            -')
+console.log('--------------------------------------')
+
+// a linha abaixo concatena (união) um texto com o
+// conteúdo da variável
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade} Anos`)
+console.log(`Peso: ${peso} Kg`)
+console.log(`Altura: ${altura}M`)
+console.log(`Vip: ${vip}`)
+console.log('--------------------------------------')
+
